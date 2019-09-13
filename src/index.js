fa649504276a0e6b55a63b09f174c3982ca21d87
Benn Thomsen <bethomse@microsoft.com>
2019-09-14 00:23:28 +0100

    Holiday image

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Travel from './travel.js';
+import Holidays from './holidays.js';
+
+ 
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <div>
+      <Holidays />
+      <Travel />
+    </div>,
+    document.getElementById('root')
+  );
+  
